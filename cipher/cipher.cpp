2a00a0dde5aa2ecd851843a42dc97f6aef46b312
Parker Nelson <pk.elliott11@gmail.com>
2025-10-02 22:13:12 -0600

    attempting my own cipher

--- cipher/cipher.cpp
+++ cipher/cipher.cpp
@@ -4,29 +4,63 @@
 #include <fstream>
 #include <sstream> 
 
-using std::cout, std::endl, std::cin, std::string, std::map, std::ifstream, std::istringstream;
+/* To handle spaces and punctuation:
+   if (c = cipher.end()) {
+      result << in[i];
+   }
+
+   Unrelated: 
+      Endless input loop:
+         while (input("Word: ", word)) {
+            blah blah blah
+         }
+
+      -- Make sure you #include "input.h"
+*/
+
+using std::cout, std::endl, std::cin, std::string, std::map, std::ifstream, std::istringstream, std::ofstream;
 
 int main() {
-   map<char, char> cipher;
+   map<char, char> aToS;
+   map<char, char> sToA;
    string alphabet = "abcdefghijklmnopqrstuvwxyz";
    string scramble = "qwzeryhtapjglndkvxoubicfms";
-   string unScrambled;
+   string line;
 
    for (size_t i = 0; i < alphabet.length(); i++) {
-      cipher[alphabet[i]] = scramble[i];
-      cipher[alphabet[i] - 'a' + 'A'] = scramble[i] - 'a' + 'A';
+      aToS[alphabet[i]] = scramble[i];
+      aToS[alphabet[i] - 'a' + 'A'] = scramble[i] - 'a' + 'A';
    }
 
-   for (auto &entry : cipher) {
-      cout << entry.first << " - " << entry.second << endl;
+   for (size_t i = 0; i < alphabet.length(); i++) {
+      sToA[scramble[i]] = alphabet[i];
+      sToA[scramble[i] - 'a' + 'A'] = alphabet[i] - 'a' + 'A';
    }
+
+   // for (auto &entry : aToS) {
+   //    cout << entry.first << " - " << entry.second << endl;
+   // }
+
+   // for (auto &entry : sToA) {
+   //    cout << entry.first << " - " << entry.second << endl;
+   // }
 
    ifstream inFile("scrambled_nemiksManifesto.txt");
-   if (!inFile) {
-      return 1;
-   }
+   if (!inFile) return 1;
 
-   while (getline(inFile, unScrambled))  {
-      
+   ofstream outFile("nemiksManifesto.txt");
+   if (!outFile) return 1;
+
+   while (getline(inFile, line))  {
+      for (char &c : line) {
+         auto key = sToA.find(c);
+         if (key != sToA.end()) {
+            cout << "Before reassignment: " << c;
+            c = key->second;
+            // cout << " - After reassignment: " << c << endl;
+         }
+      }
+      outFile << line << endl;
    }
+   cout << "Decoding complete. Output written to nemiksManifesto.txt" << endl;
 }
